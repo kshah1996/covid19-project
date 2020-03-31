bbcc91cac7bc1dcdf9737af1bff748e209b31ac4
FennecFish <euphy.w@gmail.com>
2020-03-31 12:17:11 -0400

    NB Model

--- poisson_glmm.R
+++ poisson_glmm.R
@@ -4,5 +4,10 @@ library(lme4)
 
 dat <- readRDS("dat.rds")
 
+## Poisson GLMM Model
 glmm1 <- glmer(total_cases ~ day + (day | Country.Region), data = dat, family = poisson)
 ranef(glmm1)
+
+## Negative Binomial Model 
+glmm2 <- glmer.nb(total_cases~day + (day | Country.Region), data = dat)
+ranef(glmm2)
