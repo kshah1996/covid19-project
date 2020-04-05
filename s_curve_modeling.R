2c859353f334f60eb5513784964c8677e425ed6e
Kushal Shah <31373302+kshah1996@users.noreply.github.com>
2020-04-05 19:32:07 -0400

    NLME Model

--- s_curve_modeling.R
+++ s_curve_modeling.R
@@ -6,6 +6,7 @@ library(lme4)
 library(nlme)
 
 dat <- readRDS("dat.rds")
+dat_old <- readRDS("dat_old.rds")
 
 # Works well when a country has finished its curve
 china <- filter(dat, Country.Region == "China")
@@ -63,13 +64,14 @@ plot(usa$day, usa$new_cases)
 lines(usa$day, predict(m2))
 
 # NLME
-
+ctrl <- nlmeControl(maxIter=50, msMaxIter = 50)
 baseModel <- nlme(new_cases ~ bellcurve.model(d, mu, var, x = day), 
-                  data = dat[1:2000,], 
+                  data = dat, 
                   fixed = list(d ~ 1, mu ~ 1, var ~ 1),
                   random = d + mu + var ~ 1|Country.Region,
                   start = list(fixed = c(1000, 20, 20)),
-                  na.action = na.omit)
+                  na.action = na.omit,
+                  control=ctrl)
 
 nestedModel <- update(baseModel, fixed = list(d ~ 1, mu ~ 1, var ~ 1), start = fixef(baseModel))
 nestedModel2 <- update(nestedModel, fixed = list(d ~ 1, mu ~ 1, var ~ 1), start = fixef(nestedModel))
@@ -78,3 +80,50 @@ nestedModel3 <- update(nestedModel2, fixed = list(d ~ 1, mu ~ 1, var ~ 1), start
 preds <- predict(nestedModel2)
 preds_dat <- data.frame(country = attr(preds, "names"), pred = preds)
 
+## SAEMIX
+library(saemix)
+saemix.data <- saemixData(name.data = dat,
+                          name.group = "Country.Region",
+                          name.predictors = "day",
+                          name.response = "new_cases")
+
+bellcurve.model1 <- function(psi, id, x) {
+  t <- x[,1]
+  d <- psi[id,1]
+  mu <- psi[id,2]
+  var <- psi[id,3]
+  f <- d*exp(-((t - mu)^2) / (2*var))
+  return(f)
+}
+
+saemix.model <- saemixModel(model = bellcurve.model1,
+                            psi0 = c(d = 800, mu = 20, var = 20))
+
+saemix.options <- list(map=TRUE, fim=TRUE, ll.is=FALSE, displayProgress=FALSE, seed=632545)
+saemix.fit1    <- saemix(saemix.model, saemix.data, saemix.options)
+
+
+
+
+
+
+
+
+model1cpt<-function(psi,id,xidep) { 
+  dose<-xidep[,1]
+  tim<-xidep[,2]  
+  ka<-psi[id,1]
+  V<-psi[id,2]
+  CL<-psi[id,3]
+  k<-CL/V
+  ypred<-dose*ka/(V*(ka-k))*(exp(-k*tim)-exp(-ka*tim))
+  return(ypred)
+}
+
+saemix.model<-saemixModel(model=model1cpt,
+                          description="One-compartment model with first-order absorption", 
+                          psi0=matrix(c(1.,20,0.5,0.1,0,-0.01),ncol=3, byrow=TRUE,
+                                      dimnames=list(NULL, c("ka","V","CL"))),transform.par=c(1,1,1),
+                          covariate.model=matrix(c(0,1,0,0,0,0),ncol=3,byrow=TRUE),fixed.estim=c(1,1,1),
+                          covariance.model=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE),
+                          omega.init=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE),error.model="constant")
