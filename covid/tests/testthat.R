dc4f9f5a4d12c196d02f884eb79e4caa22f54ebd
edamone <42614401+edamone@users.noreply.github.com>
2020-04-24 15:17:45 -0400

    forecast tests

---
+++ covid/tests/testthat.R
@@ -0,0 +1,4 @@
+library(covid)
+library(testthat)
+
+test_check("covid")
--- covid/tests/testthat/test-forecast.R
+++ covid/tests/testthat/test-forecast.R
@@ -1,3 +1,11 @@
 library(covid)
 
 context("test-forecast")
+
+test_that("simple warning for bad input"){
+  dat <- readRDS("dat3.rds")
+  
+  expect_error(forecast("USA", dat, as.integer(7)))
+  expect_error(forecast("US", dat, 7.6))
+  expect_error(forecast(US, dat, as.integer(7)))
+}
