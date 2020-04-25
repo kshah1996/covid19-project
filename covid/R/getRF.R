8bdcd936d886540669fcc51432eea3d9edca47ad
Kushal Shah <31373302+kshah1996@users.noreply.github.com>
2020-04-25 17:10:34 -0400

    Expanding functionality

--- covid/R/getRF.R
+++ covid/R/getRF.R
@@ -43,6 +43,7 @@ getRF <- function(Country_Name, Pred_Day = 8) {
     drop_na()
   dat2_rf_X <- dat2_rf %>% select(day, GHS_Score, AgeGEQ65, UrbanPop)
   dat2_rf_Y <- dat2_rf %>% select(new_cases)
+  library(randomForest)
   rf1 <- randomForest(x = dat2_rf_X, y = dat2_rf$new_cases)
   
   # Dataset with RF predictions
