5d14aa4cb01ab2a946a31424fd0c014c6cb935f0
tjkrajewski <tjk@live.unc.edu>
2020-04-25 16:23:49 -0400

    Fixed data preview

--- covid/tests/tests/test-countrygraph.R
+++ covid/tests/tests/test-countrygraph.R
@@ -1,8 +1,9 @@
 library(covid)
 
+context("test-countrygraph")
 
 test_that("simple warning for bad input" , {
-    dat <- readRDS("dat2.rds")
+    dat = covid2
     
     expect_error(countrygraph(21, prediction = FALSE, Pred_Day = 8), "'Country_Name' must be a character input")
     expect_error(countrygraph('US', prediction=FALSE, Pred_Day = twelve), "'Pred_Day' must be an integer input")
