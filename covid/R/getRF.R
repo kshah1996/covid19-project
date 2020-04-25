df12968da2014dffac537d0b707ca776e443702e
Kushal Shah <31373302+kshah1996@users.noreply.github.com>
2020-04-25 15:42:48 -0400

    Report update, package started

---
+++ covid/R/getRF.R
@@ -0,0 +1,51 @@
+#' Generation of a graph for any country's number of new COVID-19 cases overlayed with a Random Forest Model
+#' 
+#' This function creates a graph through a random forest prediction framework for any 
+#' user-specified country that displays number of new cases
+#' of COVID-19 from their baseline day of 50 cases confirmed until April 3, 2020,
+#' with further predictions specified by the user.
+#' 
+#' @param Country_Name A character input corresponding to the country being graphed
+#' @param Pred_Day A specified number of Days past from April 3rd the user wants to predict. Default is 8 days; only used with prediction is TRUE
+#' @param glmer_results A boolean input to display results from glmer in blue
+#' 
+#' @return A graph displaying the number of new cases for specified country in black and the GLMM model of new cases in red if prediction is specified as true, the dotted lines are the predictions.
+#' 
+#' @examples 
+#' 
+#' countrygraph(Country_Name='US')
+#' countrygraph(Country_Name='US', prediction = TRUE)
+#' countrygraph(Country_Name='China')
+#' countrygraph(Country_Name='China', prediction = TRUE)
+#' 
+#' @export
+getRF <- function(Country_Name, Pred_Day = 8) {
+  # Run RF model on full dataset
+  dat2 <- readRDS("dat2.rds") %>%
+    mutate(new_cases = replace(new_cases, Country.Region == "China" & day==0, 0))
+  dat2_rf <- dat2 %>% select(Country.Region, day, GHS_Score, AgeGEQ65, UrbanPop, new_cases) %>%
+    drop_na()
+  dat2_rf_X <- dat2_rf %>% select(day, GHS_Score, AgeGEQ65, UrbanPop)
+  dat2_rf_Y <- dat2_rf %>% select(new_cases)
+  rf1 <- randomForest(x = dat2_rf_X, y = dat2_rf$new_cases)
+  
+  # Dataset with RF predictions
+  dat2_rf_preds <- dat2_rf %>% mutate(pred = predict(rf1))
+  dat2_rf_preds_country <- dat2_rf_preds %>% filter(Country.Region == Country_Name)
+  
+  # Manually create dataset for future predictions
+  day <- 0:(max(dat2_rf_preds_country$day) + Pred_Day)
+  GHS_Score <- dat2_rf_preds_country$GHS_Score[1]
+  AgeGEQ65 <- dat2_rf_preds_country$AgeGEQ65[1]
+  UrbanPop <- dat2_rf_preds_country$UrbanPop[1]
+  
+  prediction_df <- data.frame(day, GHS_Score, AgeGEQ65, UrbanPop)
+  prediction_df$pred <- predict(rf1, prediction_df)
+  prediction_df$new_cases <- c(dat2_rf_preds_country$new_cases, rep(NA, Pred_Day))
+  
+  ggplot(prediction_df, aes(x = day)) +
+    geom_point(aes(y = new_cases)) +
+    geom_line(aes(y = pred)) +
+    labs(x = "Day", y = "New Cases") + 
+    ggtitle(paste0(Country_Name, " Predictions (RF)"))
+}
